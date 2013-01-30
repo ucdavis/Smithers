6fa34f70747928e89a7d20cfe7b85ee6341ea120
Scott Kirkland <srkirkland@gmail.com>
2013-01-30 15:04:22 -0800

    automatically send email messages when a job fails

--- Smithers.Worker/Jobs/LoggerExtensions.cs
+++ Smithers.Worker/Jobs/LoggerExtensions.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Net.Mail;
 using Common.Logging;
+using Microsoft.WindowsAzure;
+using SendGridMail;
+using SendGridMail.Transport;
+using System.Net;
 
 namespace Smithers.Worker.Jobs
 {
@@ -13,6 +18,37 @@ namespace Smithers.Worker.Jobs
             }
 
             log.ErrorFormat("Error: {0} failed at {1} with exception {2} {3}", ex, jobName, ex.Source, ex.Message, ex.StackTrace);
+
+            SendErrorEmail(jobName, ex, log);
+        }
+
+        private static void SendErrorEmail(string jobName, Exception ex, ILog log)
+        {
+            var username = CloudConfigurationManager.GetSetting("sendgrid-username");
+            var pass = CloudConfigurationManager.GetSetting("sendgrid-pass");
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(pass))
+            {
+                log.Error("Could not send email notification. Can username or password");
+            }
+
+            try
+            {
+                var sgMessage = SendGrid.GetInstance();
+                sgMessage.From = new MailAddress("caes-tech@ucdavis.edu", "Smithers Job Notifications");
+                sgMessage.Subject = "Smithers Error Notification";
+                sgMessage.AddTo("caes-tech@ucdavis.edu");
+                sgMessage.Html = string.Format("Error: {0} failed at {1} with exception {2} {3}", jobName, ex.Source,
+                                               ex.Message, ex.StackTrace);
+
+                var transport = SMTP.GetInstance(new NetworkCredential(username, pass));
+                transport.Deliver(sgMessage);
+            }
+            catch (Exception mailException)
+            {
+                log.ErrorFormat("Error: Email send failed at {0} with exception {1} {2}", mailException,
+                                mailException.Source, mailException.Message, mailException.StackTrace);
+            }
         }
     }
 }

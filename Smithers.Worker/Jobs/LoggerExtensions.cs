0a6dac5d5fd224a8b56cbca198566c71f68b42e9
Scott Kirkland <srkirkland@gmail.com>
2013-01-30 13:57:09 -0800

    added logger error exception

---
+++ Smithers.Worker/Jobs/LoggerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Common.Logging;
+
+namespace Smithers.Worker.Jobs
+{
+    public static class LoggerExtensions
+    {
+        public static void LogCustomError(this ILog log, Exception ex, string jobName = null)
+        {
+            if (jobName == null)
+            {
+                jobName = "Job";
+            }
+
+            log.ErrorFormat("Error: {0} failed at {1} with exception {2} {3}", ex, jobName, ex.Source, ex.Message, ex.StackTrace);
+        }
+    }
+}

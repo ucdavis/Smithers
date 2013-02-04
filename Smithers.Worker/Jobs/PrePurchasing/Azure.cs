cb613f4b8b5c0abac3f6b5018a284f6b2d9703a8
Scott Kirkland <srkirkland@gmail.com>
2013-02-04 09:47:26 -0800

    added initial database backup classes

---
+++ Smithers.Worker/Jobs/PrePurchasing/Azure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smithers.Worker.Jobs.PrePurchasing
+{
+    class Azure
+    {
+    }
+}
---
+++ Smithers.Worker/Jobs/PrePurchasing/DatabaseBackup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace Smithers.Worker.Jobs.PrePurchasing
+{
+    public class DatabaseBackup : Job<DatabaseBackup>
+    {
+        public static void Schedule()
+        {
+            var jobDetails = JobBuilder.Create<DatabaseBackup>().Build();
+
+            var nightly = TriggerBuilder.Create().ForJob(jobDetails).WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(1, 0)).StartNow().Build();
+            var sched = StdSchedulerFactory.GetDefaultScheduler();
+            sched.ScheduleJob(jobDetails, nightly);
+            sched.Start();
+        }
+
+        public override void ExecuteJob(IJobExecutionContext context)
+        {
+            var storageAccountName = context.MergedJobDataMap["storageAccountName"] as string;
+            var serverName = context.MergedJobDataMap["serverName"] as string;
+            var username = context.MergedJobDataMap["username"] as string;
+            var password = context.MergedJobDataMap["password"] as string;
+            var storageKey = context.MergedJobDataMap["storageKey"] as string;
+            var blobContainer = context.MergedJobDataMap["blobContainer"] as string;
+
+            var sendGridUserName = context.MergedJobDataMap["sendGridUser"] as string;
+            var sendGridPassword = context.MergedJobDataMap["sendGridPassword"] as string;
+        }
+    }
+}

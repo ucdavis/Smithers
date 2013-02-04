9a45284a1252e0b400bdd0d4ca1d24446a73017b
Scott Kirkland <srkirkland@gmail.com>
2013-02-04 09:38:47 -0800

    schedule job for verifying permissions in opp

---
+++ Smithers.Worker/Jobs/PrePurchasing/VerifyPermissions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Quartz;
+using Quartz.Impl;
+
+namespace Smithers.Worker.Jobs.PrePurchasing
+{
+    public class VerifyPermissions : Job<VerifyPermissions>
+    {
+        public static void Schedule()
+        {
+            var jobDetails = JobBuilder.Create<VerifyPermissions>().Build();
+
+            var nightly =
+                TriggerBuilder.Create().ForJob(jobDetails).WithSchedule(
+                    CronScheduleBuilder.DailyAtHourAndMinute(6, 0)).StartNow().Build();
+            var sched = StdSchedulerFactory.GetDefaultScheduler();
+            sched.ScheduleJob(jobDetails, nightly);
+            sched.Start();
+        }
+
+        public override void ExecuteJob(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

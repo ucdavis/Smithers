32026beb9149ede8e06f6826faeb3250e6214e37
Scott Kirkland <srkirkland@gmail.com>
2013-03-11 11:58:03 -0700

    added entlib transient fault application block

--- Smithers.Worker/Jobs/PrePurchasing/EmailNotificationSender.cs
+++ Smithers.Worker/Jobs/PrePurchasing/EmailNotificationSender.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Mail;
 using System.Text;
 using Dapper;
+using Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling.SqlAzure;
 using Microsoft.WindowsAzure;
 using Quartz;
 using Quartz.Impl;

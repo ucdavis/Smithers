87aa234b5c12c69a9f8ff11378961795409681c3
Scott Kirkland <srkirkland@gmail.com>
2013-03-11 15:06:26 -0700

    create new connections for each email send

--- Smithers.Worker/Jobs/PrePurchasing/EmailNotificationSender.cs
+++ Smithers.Worker/Jobs/PrePurchasing/EmailNotificationSender.cs
@@ -8,6 +8,7 @@ using System.Net.Mail;
 using System.Text;
 using Dapper;
 using Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling.SqlAzure;
+using Microsoft.Practices.TransientFaultHandling;
 using Microsoft.WindowsAzure;
 using Quartz;
 using Quartz.Impl;
@@ -75,148 +76,163 @@ namespace Smithers.Worker.Jobs.PrePurchasing
                 }
             }
         }
-        
+
         private void ProcessEmails(EmailPreferences.NotificationTypes notificationType)
         {
-            var connection = new ReliableSqlConnection(_connectionString);
-            connection.Open();
+            List<dynamic> pending, users;
 
-            using (connection)
+            using (var connection = new ReliableSqlConnection(_connectionString).Open())
             {
-                List<dynamic> pending = connection.Query(
+                pending = connection.Query(
                     "select Id, UserId, Email, OrderId, DateTimeCreated, Action, Details from EmailQueueV2 where Pending = 1 and NotificationType = @notificationType",
                     new {notificationType = notificationType.ToString()}).ToList();
 
                 var pendingUserIds = pending.Where(x => x.UserId != null).Select(x => x.UserId).Distinct();
 
-                List<dynamic> users = connection.Query("select distinct Id, Email from users where id in @ids",
-                                                       new {ids = pendingUserIds.ToArray()}).ToList();
+                users = connection.Query("select distinct Id, Email from users where id in @ids",
+                                         new {ids = pendingUserIds.ToArray()}).ToList();
 
-                #region Workgroup Notifications have a null User
-
-                var workgroupNotifications = pending.Where(b => b.UserId == null).Select(a => a.Email).Distinct();
-
-                foreach (var wEmail in workgroupNotifications)
-                {
-                    var pendingForUser = pending.Where(e => e.Email == wEmail).ToList();
-
-                    var email = wEmail;
-
-                    BatchEmail(connection, email, pendingForUser);
-                }
-
-                #endregion Workgroup Notifications have a null User
-
-                #region Normal Email Notification, user will never be null
-
-                foreach (var user in users)
-                {
-                    var pendingForUser = pending.Where(e => e.UserId == user.Id).ToList();
-
-                    var email = user.Email;
-
-                    BatchEmail(connection, email, pendingForUser);
-                }
-
-                #endregion Normal Email Notification, user will never be null
             }
+
+            #region Workgroup Notifications have a null User
+
+            var workgroupNotifications = pending.Where(b => b.UserId == null).Select(a => a.Email).Distinct();
+
+            foreach (var wEmail in workgroupNotifications)
+            {
+                var pendingForUser = pending.Where(e => e.Email == wEmail).ToList();
+
+                var email = wEmail;
+
+                BatchEmail(email, pendingForUser);
+            }
+
+            #endregion Workgroup Notifications have a null User
+
+            #region Normal Email Notification, user will never be null
+
+            foreach (var user in users)
+            {
+                var pendingForUser = pending.Where(e => e.UserId == user.Id).ToList();
+
+                var email = user.Email;
+
+                BatchEmail(email, pendingForUser);
+            }
+
+            #endregion Normal Email Notification, user will never be null
         }
 
-        private void BatchEmail(IDbConnection connection, string email, List<dynamic> pendingForUser)
+        private void BatchEmail(string email, List<dynamic> pendingForUser)
         {
             var pendingOrderIds = pendingForUser.Select(x => x.OrderId).Distinct();
-
-            //Do batches inside of their own transactions
-            using (var ts = connection.BeginTransaction())
+            
+            //Do batches inside of their own connection and transaction
+            using (var connection = new ReliableSqlConnection(_connectionString).Open())
             {
-                var pendingOrders =
-                    connection.Query(@"select o.Id, o.RequestNumber, o.OrderStatusCodeId
+                using (var ts = connection.BeginTransaction())
+                {
+                    var pendingOrders =
+                        connection.Query(@"select o.Id, o.RequestNumber, o.OrderStatusCodeId
                                         ,u.FirstName + ' ' + u.LastName as CreatedByFullName, os.Name as StatusName
                                         ,wv.Name as VendorName, wv.Line1, wv.City, wv.State, wv.Zip, wv.CountryCode
                                     from Orders o inner join Users u on u.Id = o.CreatedBy 
                                         inner join OrderStatusCodes os on os.Id = o.OrderStatusCodeId
                                         left outer join WorkgroupVendors wv on wv.Id = o.WorkgroupVendorId
                                     where o.id in @ids",
-                                new { ids = pendingOrderIds.ToArray() }, ts)
-                              .ToList();
+                                         new {ids = pendingOrderIds.ToArray()}, ts)
+                                  .ToList();
 
-                var message = new StringBuilder();
-                message.Append(string.Format("<p>{0}</p>", "Here is your summary for the PrePurchasing system."));
-                foreach (var order in pendingOrders)
-                {
-                    var extraStyle1 = string.Empty;
-                    var extraStyle2 = string.Empty;
-                    
-                    if (order.OrderStatusCodeId == "OC" || order.OrderStatusCodeId == "OD") //cancelled or denied
+                    var message = new StringBuilder();
+                    message.Append(string.Format("<p>{0}</p>", "Here is your summary for the PrePurchasing system."));
+                    foreach (var order in pendingOrders)
                     {
-                        extraStyle1 = "<span style=\"color: Red;\">";
-                        extraStyle2 = "</span>";
-                    }
-                    
-                    message.Append("<p>");
-                    message.Append("<table>");
-                    message.Append("<tbody>");
-                    message.Append(string.Format("<tr><td style=\"width: 100px;\">Order Request</td><td>{0}</td></tr>",
-                                                 GenerateLink(order.RequestNumber)));
-                    message.Append(
-                        string.Format(
-                            "<tr><td style=\"width: 100px;\"><strong>Created By:</strong></td><td>{0}</td></tr>",
-                            order.CreatedByFullName));
-                    message.Append(
-                        string.Format(
-                            "<tr><td style=\"width: 100px;\"><strong>Status:</strong></td><td>{0}{1}{2}</td></tr>",
-                            extraStyle1, order.StatusName, extraStyle2));
-                    message.Append(
-                        string.Format("<tr><td style=\"width: 100px;\"><strong>Vendor:</strong></td><td>{0}</td></tr>",
-                                      string.IsNullOrWhiteSpace(order.VendorName)
-                                          ? "-- Unspecified --"
-                                          : string.Format("{0} ({1}, {2}, {3} {4}, {5})", order.VendorName,
-                                                          order.Line1, order.City, order.State, order.Zip,
-                                                          order.CountryCode)));
-                    
-                    message.Append("</tbody>");
-                    message.Append("</table>");
-                    
-                    message.Append("<table border=\"1\">");
-                    message.Append("<tbody>");
+                        var extraStyle1 = string.Empty;
+                        var extraStyle2 = string.Empty;
 
-                    dynamic orderid = order.Id;
-                    foreach (var emailQueue in pendingForUser.Where(a => a.OrderId == orderid).OrderByDescending(b => b.DateTimeCreated))
-                    {
-                        message.Append("<tr>");
-                        message.Append(string.Format("<td style=\"padding-left: 7px; border-left-width: 0px; margin-left: 0px; width: 180px;\">{0}</td>", emailQueue.DateTimeCreated));
-                        message.Append(string.Format("<td style=\"width: 137px;\">{0}</td>", emailQueue.Action));
-                        message.Append(string.Format("<td >{0}</td>", emailQueue.Details ?? string.Empty));
-                        message.Append("</tr>");
-                        
-                        //TODO: Can move to single update outside of foreach
-                        connection.Execute("update EmailQueueV2 set Pending = 0, DateTimeSent = @now where id = @id",
-                                           new {now = DateTime.Now.InPacificTimeZone(), id = emailQueue.Id}, ts);
+                        if (order.OrderStatusCodeId == "OC" || order.OrderStatusCodeId == "OD") //cancelled or denied
+                        {
+                            extraStyle1 = "<span style=\"color: Red;\">";
+                            extraStyle2 = "</span>";
+                        }
+
+                        message.Append("<p>");
+                        message.Append("<table>");
+                        message.Append("<tbody>");
+                        message.Append(
+                            string.Format("<tr><td style=\"width: 100px;\">Order Request</td><td>{0}</td></tr>",
+                                          GenerateLink(order.RequestNumber)));
+                        message.Append(
+                            string.Format(
+                                "<tr><td style=\"width: 100px;\"><strong>Created By:</strong></td><td>{0}</td></tr>",
+                                order.CreatedByFullName));
+                        message.Append(
+                            string.Format(
+                                "<tr><td style=\"width: 100px;\"><strong>Status:</strong></td><td>{0}{1}{2}</td></tr>",
+                                extraStyle1, order.StatusName, extraStyle2));
+                        message.Append(
+                            string.Format(
+                                "<tr><td style=\"width: 100px;\"><strong>Vendor:</strong></td><td>{0}</td></tr>",
+                                string.IsNullOrWhiteSpace(order.VendorName)
+                                    ? "-- Unspecified --"
+                                    : string.Format("{0} ({1}, {2}, {3} {4}, {5})", order.VendorName,
+                                                    order.Line1, order.City, order.State, order.Zip,
+                                                    order.CountryCode)));
+
+                        message.Append("</tbody>");
+                        message.Append("</table>");
+
+                        message.Append("<table border=\"1\">");
+                        message.Append("<tbody>");
+
+                        dynamic orderid = order.Id;
+                        foreach (
+                            var emailQueue in
+                                pendingForUser.Where(a => a.OrderId == orderid)
+                                              .OrderByDescending(b => b.DateTimeCreated))
+                        {
+                            message.Append("<tr>");
+                            message.Append(
+                                string.Format(
+                                    "<td style=\"padding-left: 7px; border-left-width: 0px; margin-left: 0px; width: 180px;\">{0}</td>",
+                                    emailQueue.DateTimeCreated));
+                            message.Append(string.Format("<td style=\"width: 137px;\">{0}</td>", emailQueue.Action));
+                            message.Append(string.Format("<td >{0}</td>", emailQueue.Details ?? string.Empty));
+                            message.Append("</tr>");
+
+                            //TODO: Can move to single update outside of foreach
+                            connection.Execute(
+                                "update EmailQueueV2 set Pending = 0, DateTimeSent = @now where id = @id",
+                                new {now = DateTime.Now.InPacificTimeZone(), id = emailQueue.Id}, ts);
+                        }
+
+                        message.Append("</tbody>");
+                        message.Append("</table>");
+                        message.Append("<hr>");
+                        message.Append("</p></br>");
                     }
 
-                    message.Append("</tbody>");
-                    message.Append("</table>");
-                    message.Append("<hr>");
-                    message.Append("</p></br>");
+                    message.Append(string.Format("<p><em>{0} </em><em><a href=\"{1}\">{2}</a>&nbsp;</em></p>",
+                                                 "You can change your email preferences at any time by",
+                                                 "http://prepurchasing.ucdavis.edu/User/Profile",
+                                                 "updating your profile on the PrePurchasing site"));
+
+                    var sgMessage = SendGrid.GetInstance();
+                    sgMessage.From = new MailAddress(SendGridFrom, "UCD PrePurchasing No Reply");
+
+                    sgMessage.Subject = pendingOrders.Count == 1
+                                            ? string.Format("PrePurchasing Notification for Order #{0}",
+                                                            pendingOrders.Single().RequestNumber)
+                                            : "PrePurchasing Notifications";
+
+                    sgMessage.AddTo(email);
+                    sgMessage.Html = message.ToString();
+
+                    var transport = SMTP.GetInstance(new NetworkCredential(_sendGridUserName, _sendGridPassword));
+                    transport.Deliver(sgMessage);
+
+                    ts.Commit();
                 }
-
-                message.Append(string.Format("<p><em>{0} </em><em><a href=\"{1}\">{2}</a>&nbsp;</em></p>", "You can change your email preferences at any time by", "http://prepurchasing.ucdavis.edu/User/Profile", "updating your profile on the PrePurchasing site"));
-
-                var sgMessage = SendGrid.GetInstance();
-                sgMessage.From = new MailAddress(SendGridFrom, "UCD PrePurchasing No Reply");
-
-                sgMessage.Subject = pendingOrders.Count == 1
-                                        ? string.Format("PrePurchasing Notification for Order #{0}",
-                                                        pendingOrders.Single().RequestNumber)
-                                        : "PrePurchasing Notifications";
-
-                sgMessage.AddTo(email);
-                sgMessage.Html = message.ToString();
-
-                var transport = SMTP.GetInstance(new NetworkCredential(_sendGridUserName, _sendGridPassword));
-                transport.Deliver(sgMessage);   
-                
-                ts.Commit();
             }
         }
 
